f0ab47f8953068a6c5704f446c097c99c641f5dc
hossam2671 <108755474+hossam2671@users.noreply.github.com>
2024-04-02 04:36:40 +0200

    ,,,,

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -7,11 +7,24 @@ import axios from "axios";
 function Profile() {
   const { id } = useParams();
   const [user, setUser] = useState({});
+  const [posts, setPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:5000/user/getUser", { params: { id: id } })
       .then((res) => {
         setUser(res.data);
+      });
+    axios
+      .get("http://localhost:5000/user/userPosts", { params: { id: id } })
+      .then((res) => {
+        setPosts(res.data);
+      });
+    axios
+      .get("http://localhost:5000/user/userSavedPosts", { params: { id: id } })
+      .then((res) => {
+        setSavedPosts(res.data)
       });
   }, []);
   return (
@@ -26,24 +39,25 @@ function Profile() {
             <div className={style["name"]}>
               <h3>{user.userName}</h3>
               <h4>Edit profile</h4>
+              <h4>Log out</h4>
             </div>
             <div className={style["numbers"]}>
               <div className={style["number"]}>
-                {user.posts?.length}
+                <h3>{user.posts?.length}</h3>
                 <h4>post</h4>
               </div>
               <div className={style["number"]}>
-                {user.follwers?.length}
+                <h3>{user.follwers?.length}</h3>
                 <h4>followers</h4>
               </div>
               <div className={style["number"]}>
-                {user.follwing?.length}
+                <h3>{user.follwing?.length}</h3>
                 <h4>following</h4>
               </div>
             </div>
             <div className={style["bio"]}>
-                <h3>{user.name}</h3>
-                { user.bio && <h4>{user.bio}</h4>}
+              <h3>{user.name}</h3>
+              {user.bio && <h4>{user.bio}</h4>}
             </div>
           </div>
         </div>
